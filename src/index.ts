5743330a8396e488497a4dfa7b7c6dc8a0d0740a
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2018-09-02 14:37:48 +0200

    setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function component() {
+  let element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = "Hello :)";
+  return element;
+}
+
+document.body.appendChild(component());
